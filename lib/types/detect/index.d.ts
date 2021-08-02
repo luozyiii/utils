eca5cb8250ab58cf87692b9bd618d7386c26e684
luozyiii <924361501@qq.com>
2021-08-02 21:56:24 +0800

    构建1.0.6

---
+++ lib/types/detect/index.d.ts
@@ -0,0 +1,5 @@
+/**
+ * 判断是否是对象
+ * @param {Object} obj 对象
+ */
+export declare function isObject(obj: any): boolean;
--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -1,5 +1,7 @@
 /** reg */
 import { isIDCard, isMobile, isEmail } from './reg';
+/** detect */
+import { isObject } from './detect';
 /** params */
 import { getParamFromURL, formatUrl } from './params';
 /** number */
@@ -12,4 +14,4 @@ import { getDevice } from './device';
 import { saveBolbFile } from './file';
 /** best */
 import { debounce, throttle } from './best';
-export { isIDCard, isMobile, isEmail, getParamFromURL, formatUrl, toThousands, formatNum, sleep, getDevice, saveBolbFile, debounce, throttle, };
+export { isIDCard, isMobile, isEmail, isObject, getParamFromURL, formatUrl, toThousands, formatNum, sleep, getDevice, saveBolbFile, debounce, throttle, };
